ad07eb65b190cf141cbcf541a12c7adf29bd44b7
Rin Minase <rin.black.raison.detre@gmail.com>
2020-11-18 04:15:07 +0800

    Improved logging

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 
 const path = require('path');
-const util = require('util');
+const chalk = require('chalk');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
+const log = (input) => {
+	if (typeof input == 'string') {
+		console.log(`${chalk.blue('server')} ${input}`);
+	}
+}
+
 app
 	.use(express.static(path.join(__dirname, 'public')))
 	.listen(port, function () {
-		util.log('Server listening on port ' + port);
+		log('listening on port ' + chalk.blue(port));
 	});
