c9968cfaf8de82c253ecf2b8976bfc6f9b130ee1
Rin Minase <rin.black.raison.detre@gmail.com>
2020-11-18 05:30:10 +0800

    Appended route for power handling

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
-const chalk = require('chalk');
-const express = require('express');
+const path = require("path");
+const chalk = require("chalk");
+const express = require("express");
+const powerOff = require("power-off");
 const app = express();
 const port = process.env.PORT || 80;
 
-const log = (input) => {
-	if (typeof input == 'string') {
-		console.log(`${chalk.blue('server')} ${input}`);
+const log = (input, error) => {
+	if (typeof error == "boolean" && error) {
+		console.log(`${chalk.red("error")} ${input}`);
+	}
+
+	if (typeof input == "string") {
+		console.log(`${chalk.blue("server")} ${input}`);
 	}
 }
 
 app
-	.use(express.static(path.join(__dirname, 'public')))
-	.get('/url', (req, res) => {
-		const url = process.env.URL || '';
+	.use(express.static(path.join(__dirname, "public")))
+	.get("/url", (req, res) => {
+		const url = process.env.URL || "";
 		res.json(url);
 	})
-	.listen(port, function () {
-		log('listening on port ' + chalk.blue(port));
+	.post("/power-off", (req, res) => {
+		powerOff((err) => {
+			if (err) {
+				log(err)
+				res.status(500).json({ error: "Error in powering-off" })
+			} else {
+				res.end()
+			}
+		});
+	})
+	.listen(port, () => {
+		log("listening on port " + chalk.blue(port));
 	});
