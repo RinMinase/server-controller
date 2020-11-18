5f3c0f7dd81dc81b4f5fd7c446ac8195cd9cc64d
Rin Minase <rin.black.raison.detre@gmail.com>
2020-11-18 16:01:13 +0800

    BE: removed previous public files

--- backend/public/js/qr.js
+++
@@ -1,6 +0,0 @@
-$.get("/url", {}, (response) => {
-	let qr = qrcode(4, "L");
-	qr.addData(response);
-	qr.make();
-	document.getElementById("QR").innerHTML = qr.createImgTag(5, 8);
-});
--- backend/public/js/shutdown.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-	$("#shutdown").click(() => {
-		// $.post("/shutdown")
-	});
-});
